7a98a74f1b567535f1b61c63bcb205770a3876e5
davidearbola <davidearbola@gmail.com>
2024-08-22 11:34:03 +0200

    create model, migration, seeder and controller for Types

---
+++ app/Models/Type.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'icon'
+    ];
+
+    public function project()
+    {
+        return $this->hasMany(Project::class);
+    }
+}
